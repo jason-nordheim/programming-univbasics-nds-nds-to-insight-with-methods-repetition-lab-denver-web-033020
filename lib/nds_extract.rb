a453e3238ea6c0ff0f55e83bc0b8ad02651cc562
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-11 02:11:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,8 +48,9 @@ def total_gross(source)
   while i < ary_dir.count do
     total += dir_totals[ary_dir][:worldwide_gross]
     i += 1
-
+  end
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  return total 
 end
